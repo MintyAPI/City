5c16a379908c06f76331a3948ca5e090d71ceef1
MintyAPI <67522599+MintyAPI@users.noreply.github.com>
2020-06-28 13:20:00 +0200

    TP Command gemacht, Invsee Command fertig

--- CityBuildSystem/src/de/cb/befhle/CMD_Invsee.java
+++ CityBuildSystem/src/de/cb/befhle/CMD_Invsee.java
@@ -8,13 +8,37 @@
 
 package de.cb.befhle;
 
+import de.cb.main.CityBuild;
+import org.bukkit.Bukkit;
+import org.bukkit.Sound;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import sun.applet.Main;
 
 public class CMD_Invsee implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command CMD, String label, String[] args) {
-        return false;
+
+        if(sender instanceof Player) {
+            Player p = (Player) sender;
+            if(p.hasPermission("cb.invsee")) {
+                if(args.length == 1) {
+                    Player open_inv = Bukkit.getPlayerExact(args[0]);
+                    if(open_inv != null) {
+                        p.openInventory(open_inv.getInventory());
+                    }else {
+                        p.sendMessage(CityBuild.Prefix + "§cDieser Spieler ist nicht auf dem Server");
+                        p.sendTitle("§7Dieser Spieler ist", "§cnicht §7auf dem Server");
+                        p.playSound(p.getLocation(), Sound.BAT_HURT,1,1);
+                    }
+                }else {
+                    p.sendMessage(CityBuild.Prefix + "§c/invsee <PLAYER>");
+                    p.sendTitle("§7Bitte benutze", "§c/invsee <PLAYER>");
+                    p.playSound(p.getLocation(), Sound.BAT_HURT,1,1); }
+            }
+        }
+        return true;
     }
 }
--- CityBuildSystem/src/de/cb/befhle/CMD_TP.java
+++ CityBuildSystem/src/de/cb/befhle/CMD_TP.java
@@ -8,13 +8,63 @@
 
 package de.cb.befhle;
 
+import de.cb.main.CityBuild;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Sound;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class CMD_TP implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command CMD, String label, String[] args) {
+
+        Player p = (Player)sender;
+        if(p.hasPermission("cb.tp")) {
+
+        if(!(sender instanceof Player)){
+
+            sender.sendMessage("§cERROR | Du bist kein Spieler");
+
+        }
+
+        if(args.length == 0){
+
+
+            p.sendMessage(CityBuild.Prefix + ChatColor.GRAY + "Bitte benutze: §c/tp <NAME>");
+            p.sendTitle("§7Bitte benutze", "§c/tp <NAME>");
+            p.playSound(p.getLocation(), Sound.BAT_HURT,1,1);
+
+        }
+        if(args.length == 1) {
+
+            Player target = Bukkit.getServer().getPlayer(args[0]);
+
+            p.teleport(target.getLocation());
+
+            p.sendMessage(CityBuild.Prefix + ChatColor.GRAY + "Du wurdest erfolgreich zu " + ChatColor.RED + target.getName() + " §7teleportiert");
+            p.sendTitle("§7Du wurdest erfolgreich zu", ChatColor.RED + target.getName() + " §7Teleportiert");
+            p.playSound(p.getLocation(), Sound.ENDERMAN_TELEPORT,1,1);
+
+        }
+
+            if(args.length >= 2) {
+
+            p.sendMessage(CityBuild.Prefix + ChatColor.GRAY + "Bitte benutze §c/tp <NAME>");
+            p.sendTitle("§7Bitte benutze", "§c/tp <NAME>");
+            p.playSound(p.getLocation(), Sound.BAT_HURT,1,1);
+
+
+        }
+
+
+
+
+
+
+
+
         return false;
     }
-}
